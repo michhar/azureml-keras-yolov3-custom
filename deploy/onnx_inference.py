6fd6cdc59fdf83bb45a4c25d11420e3c5312ed5e
Micheleen Harris <michhar@microsoft.com>
2020-07-06 08:14:04 -0700

    Get complete network results for ONNX conversion

--- deploy/onnx_inference.py
+++ deploy/onnx_inference.py
@@ -1,34 +1,182 @@
 """
-Quick ONNX inference test to ensure model loads and performs inference.
+ONNX inference test to ensure model loads and performs inference.
+Returns bounding identified boxes.  WIP.
 """
-
-
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input
+from keras import backend as K
+from PIL import Image
+import tensorflow as tf
+import cv2
 import onnxruntime
 import numpy as np
 import argparse
 from timeit import default_timer as timer
+import onnx
 
 
 def main(args):
-    # image preprocessing
-    img_path = args.image   # make sure the image is in img_path
-    img_size = 416
-    img = image.load_img(img_path, target_size=(img_size, img_size))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    """
+    Prints bounding boxes and class.
 
-    # runtime prediction
+    Arguments
+    ---
+    args : dict
+        args.image
+        args.model_size
+        args.model_local
+    """
+    # Image preprocessing
+    img_path = args.image   # manpe sure the image is in img_path
+    # TODO: check model size w/ assert statement
+    img_size = args.model_size
+    # img = image.load_img(img_path, target_size=(img_size, img_size))
+    img = Image.open(img_path)
+    orig_size = img.size
+    img = img.resize((img_size, img_size))
+    x = np.array(img, dtype='float32')
+    x /= 255.
+    x = np.expand_dims(x, 0)
+
+    # Check model
+    model = onnx.load(args.model_local)
+    onnx.checker.check_model(model)
+    ## To print graph of ONNX model, uncomment the following line
+    # print(onnx.helper.printable_graph(model.graph))
+    
+    # Runtime prediction
     sess = onnxruntime.InferenceSession(args.model_local)
     x = x if isinstance(x, list) else [x]
     feed = dict([(input.name, x[n]) for n, input in enumerate(sess.get_inputs())])
     start = timer()
-    pred_onnx = sess.run(None, feed)
+    outputs = sess.run(None, feed)
     end = timer()
     print('{} seconds for inference with ONNX runtime'.format((end-start)))
 
+    # Process ONNX model output (output conv layers)
+    boxes, scores, classes = yolo_eval(outputs, 
+                # TODO: read in an anchor file instead of hardcoding for test
+                anchors=np.array([[36,41], [62,73], [93,105], [135,156], [212,232], [379,433]]),
+                num_classes=2, 
+                image_shape=[416, 416],
+                score_threshold=0.5,
+                iou_threshold=0.3)
+
+    # Tensor to float and print
+    # TODO:  process box - such that relative to original image size
+    boxes = K.eval(boxes)
+    scores = K.eval(scores)
+    classes = K.eval(classes)
+    for i, c in enumerate(classes):
+        score = scores[i]
+        box = boxes[i]
+        print(box, ' ', c)
+
+def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
+    """Convert final layer features to bounding box parameters."""
+    feats = K.constant(feats)
+    num_anchors = len(anchors)
+    # Reshape to batch, height, width, num_anchors, box_params.
+    anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, num_anchors, 2])
+
+    grid_shape = K.shape(feats)[1:3] # height, width
+    grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
+        [1, grid_shape[1], 1, 1])
+    grid_x = K.tile(K.reshape(K.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]),
+        [grid_shape[0], 1, 1, 1])
+    grid = K.concatenate([grid_x, grid_y])
+    grid = K.cast(grid, K.dtype(feats))
+
+    feats = K.reshape(
+        feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
+
+    # Adjust preditions to each spatial grid point and anchor size.
+    box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[::-1], K.dtype(feats))
+    box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[::-1], K.dtype(feats))
+    box_confidence = K.sigmoid(feats[..., 4:5])
+    box_class_probs = K.sigmoid(feats[..., 5:])
+
+    if calc_loss == True:
+        return grid, feats, box_xy, box_wh
+    return box_xy, box_wh, box_confidence, box_class_probs
+
+def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
+    """Get corrected boxes"""
+    box_yx = box_xy[..., ::-1]
+    box_hw = box_wh[..., ::-1]
+    input_shape = K.cast(input_shape, K.dtype(box_yx))
+    image_shape = K.cast(image_shape, K.dtype(box_yx))
+    new_shape = K.round(image_shape * K.min(input_shape/image_shape))
+    offset = (input_shape-new_shape)/2./input_shape
+    scale = input_shape/new_shape
+    box_yx = (box_yx - offset) * scale
+    box_hw *= scale
+
+    box_mins = box_yx - (box_hw / 2.)
+    box_maxes = box_yx + (box_hw / 2.)
+    boxes =  K.concatenate([
+        box_mins[..., 0:1],  # y_min
+        box_mins[..., 1:2],  # x_min
+        box_maxes[..., 0:1],  # y_max
+        box_maxes[..., 1:2]  # x_max
+    ])
+
+    # Scale boxes back to original image shape.
+    boxes *= K.concatenate([image_shape, image_shape])
+    return boxes
+
+def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape):
+    """Process Conv layer output"""
+    box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats,
+        anchors, num_classes, input_shape)
+    boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
+    boxes = K.reshape(boxes, [-1, 4])
+    box_scores = box_confidence * box_class_probs
+    box_scores = K.reshape(box_scores, [-1, num_classes])
+    return boxes, box_scores
+
+def yolo_eval(yolo_outputs,
+              anchors,
+              num_classes,
+              image_shape,
+              max_boxes=20,
+              score_threshold=.6,
+              iou_threshold=.5):
+    """Evaluate YOLO model on given input and return filtered boxes."""
+    num_layers = len(yolo_outputs)
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]] # default setting
+    input_shape = K.shape(yolo_outputs[0])[1:3] * 32
+    boxes = []
+    box_scores = []
+    for l in range(num_layers):
+        _boxes, _box_scores = yolo_boxes_and_scores(yolo_outputs[l],
+            anchors[anchor_mask[l]], num_classes, input_shape, image_shape)
+        boxes.append(_boxes)
+        box_scores.append(_box_scores)
+    boxes = K.concatenate(boxes, axis=0)
+    box_scores = K.concatenate(box_scores, axis=0)
+
+    mask = box_scores >= score_threshold
+    max_boxes_tensor = K.constant(max_boxes, dtype='int32')
+    boxes_ = []
+    scores_ = []
+    classes_ = []
+    for c in range(num_classes):
+        # TODO: use keras backend instead of tf.
+        class_boxes = tf.boolean_mask(boxes, mask[:, c])
+        class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
+        nms_index = tf.image.non_max_suppression(
+            class_boxes, class_box_scores, max_boxes_tensor, iou_threshold=iou_threshold)
+        class_boxes = K.gather(class_boxes, nms_index)
+        class_box_scores = K.gather(class_box_scores, nms_index)
+        classes = K.ones_like(class_box_scores, 'int32') * c
+        boxes_.append(class_boxes)
+        scores_.append(class_box_scores)
+        classes_.append(classes)
+    boxes_ = K.concatenate(boxes_, axis=0)
+    scores_ = K.concatenate(scores_, axis=0)
+    classes_ = K.concatenate(classes_, axis=0)
+
+    return boxes_, scores_, classes_
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -40,6 +188,11 @@ if __name__ == "__main__":
     parser.add_argument(
         '--image', type=str, dest='image',
         help='The path to a test image.'
+    )
+    parser.add_argument(
+        '--model-size', type=int, dest='model_size',
+        help='The model input size, either 416 or 608.  \
+            The tiny version is 416 and full-size is 608.'
     )
 
     args = parser.parse_args()
